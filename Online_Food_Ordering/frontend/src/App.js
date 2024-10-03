b4c904980e515f03e597938fc4f434c43dfcc0ff
nitishkhobragade <nitishkhobragade89@gmail.com>
2024-10-03 10:31:53 +0530

    header created

---
+++ Online_Food_Ordering/frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './component/Navbar/Navbar';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from './Theme/DarkTheme';
+import Home from './component/Home/Home';
+
+function App() {
+  return (
+    
+      <ThemeProvider theme={darkTheme} >
+
+        <CssBaseline />
+        <Navbar />
+        <Home/>
+        
+      </ThemeProvider>
+  );
+}
+
+export default App;
